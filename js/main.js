d7748412ce86cbe5ab3d829d6424a198f3e51e3e
Carla Zafirakos <caza1000@stud.ek.dk>
2026-04-23 13:19:41 +0200

    form med opsummering

--- js/main.js
+++ js/main.js
@@ -18,3 +18,36 @@ function getSavedTheme() {
 
 getSavedTheme();
 toggleSwitch?.addEventListener("change", handleThemeChange);
+
+const form = document.querySelector("#webform");
+const summary = document.querySelector("#form-summary article");
+
+// slider live update
+document.querySelector("#slider").addEventListener("input", function () {
+  document.querySelector("#slider-value").textContent = this.value;
+});
+
+// submit
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const foodtype =
+    document.querySelector('input[name="foodtype"]:checked')?.value ||
+    "Ikke valgt";
+
+  const budget = document.querySelector("#slider").value;
+  const madvare = document.querySelector("#madvare").value || "Ikke angivet";
+
+  const personer =
+    [...document.querySelectorAll('input[type="checkbox"]:checked')]
+      .map((el) => el.value)
+      .join(", ") || "Ingen valgt";
+
+  summary.innerHTML = `
+    
+    <p><strong>Måltid:</strong> ${foodtype}</p>
+    <p><strong>Budget:</strong> ${budget} kr</p>
+    <p><strong>Madvarer:</strong> ${madvare}</p>
+    <p><strong>Personer:</strong> ${personer}</p>
+  `;
+});
